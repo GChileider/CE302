996d40fbd4fc6b543fac1900f69b5981f0908039
GChileider <gabriel.chileider@ufpr.br>
2024-10-05 18:10:42 -0300

    Adicionando novas funcionalidades

--- LOUCURAS.R
+++ LOUCURAS.R
@@ -3,3 +3,15 @@ mean(dados)
 
 sd(dados)
 var(dados)
+
+median(dados)
+
+
+library(usethis)
+install.packages(usethis)
+install.packages("usethis")
+library(usethis)
+
+use_git_config(user.name = "GChileider",
+               user.email = "gabriel.chileider@ufpr.br")
+
